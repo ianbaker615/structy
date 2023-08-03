ecb1c336197d61b5e4ded3005c8e181e620e811f
Ian Baker <ianbaker615@gmail.com>
2023-08-03 13:55:44 -0700

    feat: linked_palindrome solution and test

---
+++ mixed_recall/linked_palindrome.py
@@ -0,0 +1,37 @@
+"""
+Write a function, linked_palindrome, that takes in the head of a linked list as an argument. The function should return
+a boolean indicating whether or not the linked list is a palindrome.
+A palindrome is a sequence that is the same both forwards and backwards.
+"""
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+def linked_palindrome(head):
+    values = []
+    current = head
+    while current is not None:
+        values.append(current.val)
+        current = current.next
+
+    return values == values[::-1]
+
+
+a = Node(3)
+b = Node(2)
+c = Node(7)
+d = Node(7)
+e = Node(2)
+f = Node(3)
+
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+e.next = f
+
+assert linked_palindrome(a) is True
